90f72c0b56c2c4e813cbbf5a3845cb0491eae985
Dan Drennan <danjdrennan@gmail.com>
2024-10-24 00:11:28 -0500

    theme: update status line

--- nvim/init.lua
+++ nvim/init.lua
@@ -232,7 +232,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'catppuccin-mocha',
+        theme = 'no-clown-fiesta',
         component_separators = '|',
         section_separators = '',
       },
