f06839cd56f014a29546c6f63fb1c3ff744845e5
Dan Drennan <danjdrennan@gmail.com>
2024-10-24 23:04:02 -0500

    add cmake to treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -450,7 +450,7 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'bash', 'c', 'cpp', 'lua', 'python', 'rust', 'vimdoc', 'vim' },
+    ensure_installed = { 'bash', 'c', 'cmake', 'cpp', 'lua', 'python', 'rust', 'vimdoc', 'vim' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
