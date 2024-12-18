845d89def33304fd3873298995439bce44a52638
Dan Drennan <danjdrennan@gmail.com>
2024-12-18 01:53:34 -0600

    formatting: c and cpp file formatting defaults

---
+++ nvim/ftplugin/c.lua
@@ -0,0 +1,7 @@
+-- ~/.config/nvim/ftplugin/c.lua
+vim.opt_local.spell = true
+vim.opt_local.textwidth = 80
+vim.opt_local.wrap = true
+vim.opt_local.wrapmargin = 2
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
