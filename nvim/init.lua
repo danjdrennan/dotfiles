f08d7769e4e9b11b6d7c390fc34835ce0d92ef33
Dan Drennan <danjdrennan@gmail.com>
2024-12-23 15:29:44 -0600

    bugfix: hl removed from vim opts?

--- nvim/init.lua
+++ nvim/init.lua
@@ -360,7 +360,7 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
-    vim.hl.on_yank()
+    vim.highlight.on_yank()
   end,
   group = highlight_group,
   pattern = '*',
