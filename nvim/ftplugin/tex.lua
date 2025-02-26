9cd007775dcc50b5fd56a224b58567e50046e0e2
Dan Drennan <danjdrennan@gmail.com>
2025-02-25 22:23:38 -0600

    Reapply "copy some of rust opts into text files"
    This reverts commit 0b8c55cf07af72701b527d55ba75e1112db6b307.
    
    Actually I preferred it the way it was tbh.

--- nvim/ftplugin/tex.lua
+++ nvim/ftplugin/tex.lua
@@ -1,7 +1,8 @@
 -- ~/.config/nvim/ftplugin/tex.lua
 vim.opt_local.spell = true
-vim.opt_local.textwidth = 80
+vim.opt_local.textwidth = 100
 vim.opt_local.wrap = false
 vim.opt_local.wrapmargin = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.tabstop = 2
+vim.opt_local.colorcolumn = "100"
