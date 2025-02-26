b04388e8cedf41cb5796c3313a0cf903ff8d4ce8
Dan Drennan <danjdrennan@gmail.com>
2025-02-25 22:18:25 -0600

    config local opts for rust programs

---
+++ nvim/ftplugin/rust.lua
@@ -0,0 +1,8 @@
+-- ~/.config/nvim/ftplugin/rust.lua
+vim.opt_local.spell = true
+vim.opt_local.textwidth = 100
+vim.opt_local.wrap = false
+vim.opt_local.wrapmargin = 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+vim.opt_local.colorcolumn = "100"
