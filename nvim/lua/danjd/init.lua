5dc5628754cca440ee7bcd75a418d14aa5b645e3
Dan Drennan <danjdrennan@gmail.com>
2025-05-28 11:54:30 -0500

    dead code remove

--- nvim/lua/danjd/init.lua
+++ nvim/lua/danjd/init.lua
@@ -3,7 +3,6 @@ require("danjd.set")
 require("danjd.harpoon")
 
 local augroup = vim.api.nvim_create_augroup
-local autocmd = vim.api.nvim_create_autocmd
 
 local BufferAutos = augroup('BufferAutos', {})
 
