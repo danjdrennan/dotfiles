8b9265742c15b0091abf6d0ce3b65c2c0420f4f9
Dan Drennan <danjdrennan@gmail.com>
2025-07-16 01:27:26 -0500

    sure

---
+++ nvim/ftplugin/zig.lua
@@ -0,0 +1,8 @@
+-- ~/.config/nvim/ftplugin/zig.lua
+vim.opt_local.spell = true
+vim.opt_local.textwidth = 80
+vim.opt_local.wrap = false
+vim.opt_local.wrapmargin = 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+vim.opt_local.colorcolumn = "80"
