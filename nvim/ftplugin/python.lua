4a5a2b078d9a76b6023872d3fe4d0ccfae22f3da
Dan Drennan <danjdrennan@gmail.com>
2025-07-19 19:59:16 -0500

    shrink python formatting reqs

--- nvim/ftplugin/python.lua
+++ nvim/ftplugin/python.lua
@@ -2,7 +2,7 @@
 vim.opt_local.spell = true
 vim.opt_local.textwidth = 88
 vim.opt_local.wrap = false
-vim.opt_local.wrapmargin = 4
-vim.opt_local.shiftwidth = 4
-vim.opt_local.tabstop = 4
+vim.opt_local.wrapmargin = 2
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
 vim.opt_local.colorcolumn = "88"
