a4b1d15a9008ca327f41e4a3b0e34f5712ef3120
Dan Drennan <danjdrennan@gmail.com>
2025-09-23 13:10:28 -0500

    py: trying to fix python default config

--- nvim/ftplugin/py.lua
+++ nvim/ftplugin/py.lua
@@ -1,8 +1,11 @@
 -- ~/.config/nvim/ftplugin/py.lua
 vim.opt_local.spell = true
-vim.opt_local.textwidth = 88
+vim.opt_local.textwidth = 80
 vim.opt_local.wrap = false
 vim.opt_local.wrapmargin = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.tabstop = 2
+vim.opt_local.expandtab = true
 vim.opt_local.colorcolumn = "88"
+
+vim.b.sleuth_automatic = 0
