339b65a1b2b5b38674222c3d36df855e1b5211da
Dan Drennan <danjdrennan@gmail.com>
2025-09-23 13:07:34 -0500

    rm vim-sleuth
    This messes with the tab and indentation settings of the editor when I'd
    prefer not to do that. I can configure my own settings for this.

--- nvim/init.lua
+++ nvim/init.lua
@@ -32,9 +32,6 @@ require('lazy').setup({
   -- Git related plugins
   'tpope/vim-fugitive',
 
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
-
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
