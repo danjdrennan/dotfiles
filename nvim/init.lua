dcbaa3581c7344acc80390fafdb48fdbf3304444
Dan Drennan <danjdrennan@gmail.com>
2026-02-18 09:15:27 -0600

    remove some comments

--- nvim/init.lua
+++ nvim/init.lua
@@ -419,11 +419,12 @@ end, 150)
 require('mason').setup()
 require('mason-lspconfig').setup()
 
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+-- Enable the following language servers Feel free to add/remove any LSPs that
+-- you want here. They will automatically be installed.
 --
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
+--  Add any additional override configuration in the following tables. They
+--  will be passed to the `settings` field of the server config. You must look
+--  up that documentation yourself.
 local servers = {
   clangd = {},
   texlab = {},
