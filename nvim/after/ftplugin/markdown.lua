9f19ade7cb1cbfefa2e5627d3345d958d81d5363
Dan Drennan <danjdrennan@gmail.com>
2026-04-28 01:44:47 -0500

    nvim(ftplugin): set 2 space shiftwidth etc as default in more files

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,4 +1,7 @@
 -- No LSP formatter configured for markdown; these settings drive display
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
 vim.opt_local.spell = true
 vim.opt_local.textwidth = 72
 vim.opt_local.colorcolumn = "72"
--- nvim/after/ftplugin/tex.lua
+++ nvim/after/ftplugin/tex.lua
@@ -1,4 +1,7 @@
 -- Display settings; texlab provides formatting via latexindent
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
 vim.opt_local.spell = true
 vim.opt_local.textwidth = 72
 vim.opt_local.colorcolumn = "72"
